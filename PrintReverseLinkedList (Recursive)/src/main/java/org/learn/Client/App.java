0b9db6b802cbdab9408a535e2e424562418257e8
makeinjava <makeinjava@gmail.com>
2016-08-15 23:33:28 +0530

    Updated code

--- PrintReverseLinkedList (Recursive)/src/main/java/org/learn/Client/App.java
+++ PrintReverseLinkedList (Recursive)/src/main/java/org/learn/Client/App.java
@@ -3,17 +3,18 @@ package org.learn.Client;
 import org.learn.List.Node;
 import org.learn.Question.PrintReverseLinkedList;
 
-public class App 
-{
-	public static void main( String[] args )
-    {
-		int[] data = {1, 2, 3, 4, 5};
+public class App {
+	public static void main(String[] args) {
+		int[] data = { 1, 2, 3, 4, 5 };
 		Node head = new Node(data[0]);
-		for(int count = 1; count < data.length; count++)
-			PrintReverseLinkedList.insert(head,data[count]);
-		System.out.println("Original Linked list");
+
+		for (int count = 1; count < data.length; count++)
+			PrintReverseLinkedList.insert(head, data[count]);
+
+		System.out.println("1. Original Single linked list : ");
 		PrintReverseLinkedList.print(head);
-		System.out.println("Printing the linked list in reverse order");
+
+		System.out.println("2. Printing single linked list in reverse order : ");
 		PrintReverseLinkedList.printReverseLinkedList(head);
-    }
+	}
 }
--- PrintReverseLinkedList (Recursive)/src/main/java/org/learn/Question/PrintReverseLinkedList.java
+++ PrintReverseLinkedList (Recursive)/src/main/java/org/learn/Question/PrintReverseLinkedList.java
@@ -4,24 +4,25 @@ import org.learn.List.Node;
 
 public class PrintReverseLinkedList {
 	public static void printReverseLinkedList(Node head) {
-		if(null == head) {
+		if (null == head) {
+			
 			return;
-		}		
+		}
 		printReverseLinkedList(head.next);
-		System.out.printf("%d ",head.data);		
+		System.out.printf("%d ", head.data);
 	}
-	
+
 	public static void insert(Node head, int data) {
-		while(head.next != null) 
+		while (head.next != null)
 			head = head.next;
 		head.next = new Node(data);
 	}
-	
+
 	public static void print(Node head) {
-		while(head != null) {
-			System.out.printf("%d ",head.data);
+		while (head != null) {
+			System.out.printf("%d ", head.data);
 			head = head.next;
 		}
-		System.out.println("");		
+		System.out.println("");
 	}
 }
